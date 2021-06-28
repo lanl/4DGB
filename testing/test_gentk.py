a8a8cdc0954f203042bea4c543c943ba266562f7
David Honegger Rogers <dhr@lanl.gov>
2021-06-28 10:57:35 -0600

    adding assert

--- testing/test_gentk.py
+++ testing/test_gentk.py
@@ -4,5 +4,11 @@ def test_client():
     client = gentk.client.client("http://127.0.0.1", "8000")
     client.project = "test.00"
 
-    print(client.get_structure(0))
+    gold = {'end': [0.0, 0.0, 0.0], 
+            'length': 200000, 
+            'segid': 1, 
+            'start': [-1.0, 0.0, 0.0], 
+            'startid': 0}
+    result = client.get_structure(0)
 
+    assert(result['segments'][0] == gold)
