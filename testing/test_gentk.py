e5c75afc4954584502d9a6108b4656f5a05ea0af
ricardo sida <rsida2@miners.utep.edu>
2021-07-15 12:43:21 -0600

    true

--- testing/test_gentk.py
+++ testing/test_gentk.py
@@ -22,7 +22,7 @@ def test_genes():
 
 '''
 def test_genes_for_segment():
-    result = client.get_genes_for_segment(0,1)
+    result = client.get_genes_for_segment(0,0)
     ogResult = []
     assert (result['genes'] == ogResult)
     
