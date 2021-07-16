f0aaa66044327eef63f599b684bfcbf3935c5ebf
ricardo sida <rsida2@miners.utep.edu>
2021-07-16 11:33:33 -0600

    test genes

--- testing/test_gentk.py
+++ testing/test_gentk.py
@@ -2,7 +2,7 @@ import gentk
 
 client = gentk.client.client("http://127.0.0.1", "8000")
 client.project = "test.00"
-
+'''
 def test_segments():
     gold = {'end': [0.0, 0.0, 0.0], 
             'length': 200000, 
@@ -11,7 +11,7 @@ def test_segments():
             'startid': 0}
     result = client.get_structure(0)
     assert(result['segments'][0] == gold)
-
+'''
 
 def test_genes(): 
     
