c818fd4c0675951effffdfdb9ec2b10e7b0544be
David Honegger Rogers <dhr@lanl.gov>
2021-07-20 16:05:07 -0600

    update

--- testing/client.test.js
+++ testing/client.test.js
@@ -7,25 +7,14 @@ test('client test', () => {
     // test.00
     client = new GTKClient("http://127.0.0.1", 8000);
 
-    // test.01
-    // client = new GTKClient("http://127.0.0.1", 8001);
-
-    streamname = './client_output.txt'
-    var writeStream = fs.createWriteStream(streamname, {flags: 'w'});
-    writeStream.write("Client output\n\n");
+    var writeStream = fs.createWriteStream("client_genes.json", {flags: 'w'});
     writeStream.end();
+    client.get_genes( (response) => {
+                            var writeStream = fs.createWriteStream(streamname, {flags: 'a'});
+                            writeStream.write(JSON.stringify(response));
+                        });
 
-    if (true) {
-        client.get_genes( (response) => {
-                                var writeStream = fs.createWriteStream(streamname, {flags: 'a'});
-                                writeStream.write("call: genes\n");
-                                writeStream.write(JSON.stringify(response));
-                                writeStream.write("\n");
-                                writeStream.end();
-                            });
-    }
-
-    if (true) {
+    if (false) {
         client.get_genes_for_segment( (response) => {
                                 var writeStream = fs.createWriteStream(streamname, {flags: 'a'});
                                 writeStream.write("call: genes for segment\n");
@@ -35,7 +24,7 @@ test('client test', () => {
                             }, 0, 100);
     }
 
-    if (true) {
+    if (false) {
         client.get_structure( (response) => {
                                 var writeStream = fs.createWriteStream(streamname, {flags: 'a'});
                                 writeStream.write("call: structure\n");
