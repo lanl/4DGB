e89f732f1bc2b6dbf52906b87adb49e2690dda0a
David Honegger Rogers <dhr@lanl.gov>
2021-07-30 13:28:51 -0600

    fixing bug in gentk setup

--- src/gentk/setup.py
+++ src/gentk/setup.py
@@ -4,7 +4,7 @@ long_description = open('readme.md').read()
 
 setuptools.setup(
     name="gentk",
-    version="0.5.11",
+    version="0.5.12",
     author="David H. Rogers",
     author_email="dhr@lanl.gov",
     description="4D Genome Toolkit.",
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/lanl/4DGB",
     include_package_data=True,
+    package_data={'': ['license.md', 'readme.md']},
     packages=[  "gentk", 
                 "gentk.client",
                 "gentk.project"
@@ -20,7 +21,7 @@ setuptools.setup(
         "pypac==0.15.0"
     ],
     scripts=[
-        "bin/gentk"
+        "genex"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
