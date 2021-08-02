2e31a7af0f2c5a9823bd3fd4774845287c29faaa
David Honegger Rogers <dhr@lanl.gov>
2021-08-02 13:26:19 -0600

    upping build version

--- src/gentk/setup.py
+++ src/gentk/setup.py
@@ -4,7 +4,7 @@ long_description = open('readme.md').read()
 
 setuptools.setup(
     name="gentk",
-    version="0.5.13",
+    version="0.5.14",
     author="David H. Rogers",
     author_email="dhr@lanl.gov",
     description="4D Genome Toolkit.",
