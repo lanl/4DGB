38de692cfe58a9fe2c3b7fadd6505fe633f19a92
ricardo sida <rsida2@miners.utep.edu>
2021-08-13 16:27:11 -0600

    changes to notebook

--- doc/examples/001/functions.py
+++ doc/examples/001/functions.py
@@ -1,24 +1,5 @@
-import pandas as pd, numpy as np, glob, json, sys
-from pypac import PACSession
-
 
 ## -------------------- JSON visulization ftns --------------------- ##
-## Write ftn to get json data
-def get_structure(url,port,eid,verbose=False):
-    """Given a url and port, return the segment identified by eid from 4D genome project"""
-    
-    # respect local proxy settings
-    session = PACSession()
-    
-    if verbose:
-        print("eid: {}".format(eid))
-    
-    response = session.get('{}:{}/data/structure/{}/segments'.format(url, port, eid))
-    
-    if verbose:
-        print(response.text)
-        
-    return json.loads(response.text)
 
 ## Write ftn to iterate over rows of json data
 def parse_json(jdata):
