45660e23918fcc74d51795952a621a660360b598
David Honegger Rogers <dhr@lanl.gov>
2021-09-24 12:12:24 -0600

    added tube geometry

--- client-js/GTK/CurveSegment.js
+++ client-js/GTK/CurveSegment.js
@@ -38,6 +38,31 @@ const Segment = require('./Segment')
  */
 class CurveSegment extends Segment {
 
+    static CurveType            = "catmullrom";
+    static CurveClosed          = false;
+    static CurveGeomType        = "TUBE";
+    static CurveTension         =  1.0; 
+    static CurveInitNumpoints   = 23;
+    static CurveFinalNumpoints  = 10;
+    static SegmentRadius        =  0.05;
+    static SegmentNumpoints     = 10;
+    static SegmentNumSections   =  8;
+    static Linewidth            =  5;
+
+    //
+    // constructor is handed four points:
+    // 0 - point before this segment
+    // 1 - first point of segment
+    // 2 - second point of segment
+    // 3 - point after this segment
+    //
+    // The segment will construct geometry that spans from 1-2, and is as continuous 
+    // as possible with the previous segment.
+    //
+    // Limitations within three.js make it impossible to create geometry between
+    // points 1&2 of the curve, even though this is the definition of the camull rom
+    // curve. Instead, we use other methods to create this same geometry.
+    //
     constructor( ID, points, radius ) {
         super();
         const newMat = new THREE.MeshPhongMaterial({color: Segment.Color});
@@ -57,35 +82,64 @@ class CurveSegment extends Segment {
     }
 
     //
-    // create a catmull-rom curve 
+    // create curve geometry between the middle two points of a catmull rom curve 
     //
     createSpanMesh( points, radius, material) {
-        var type        = "catmullrom";
-        var closed      = false;
-        var tension     = 1.0; 
-        var numpoints   = 23;
+        if (CurveSegment.CurveGeomType == "LINE") {
+            // copy the material
+            var newMaterial = new THREE.LineBasicMaterial( {
+                        color: Segment.Color,
+                        linewidth: 5
+            });
 
-        // copy the material
-        var newMaterial = new THREE.LineBasicMaterial( {
-	                color: Segment.Color,
-	                linewidth: 5
-        });
+            // make the geometry
+            var curve    = new THREE.CatmullRomCurve3( [ points['0'], points['1'], points['2'], points['3'] ], closed, type, tension ); 
+            var cmPoints = curve.getPoints(numpoints);
+            var p = this.findMidsection( cmPoints, points['1'], points['2'] );
+            var buffGeom = new THREE.BufferGeometry().setFromPoints(p);
+            this.spanMesh = new THREE.Line( buffGeom, newMaterial );
 
-        // make the geometry
-        var curve    = new THREE.CatmullRomCurve3( [ points['0'], points['1'], points['2'], points['3'] ], closed, type, tension ); 
-        var cmPoints = curve.getPoints(numpoints);
-        var p = this.findMidsection( cmPoints, points['1'], points['2'] );
-        var buffGeom = new THREE.BufferGeometry().setFromPoints(p);
-        this.spanMesh = new THREE.Line( buffGeom, newMaterial );
+            this.root.add(this.spanMesh);
 
-        this.root.add(this.spanMesh);
+        } else {
+            var newMaterial = new THREE.MeshPhongMaterial({color: Segment.Color});
+
+            // make geometry, using three.js curves 
+            // this creates points along the entire curve (0 through 3)
+            var curve    = new THREE.CatmullRomCurve3( [ points['0'], points['1'], points['2'], points['3'] ], 
+                                                        CurveSegment.CurveClosed, CurveSegment.CurveType, CurveSegment.CurveTension ); 
+            var cmPoints = curve.getPoints(CurveSegment.CurveInitNumpoints);
+
+            // get the section of the curve between the 'start' and 'end' points
+            var p = this.findMidsection( cmPoints, points['1'], points['2'] );
+                // create a new curve
+            var finalCurve = new THREE.CatmullRomCurve3( p, CurveSegment.CurveClosed, CurveSegment.CurveType, CurveSegment.CurveTension ); 
+            var finalPoints = finalCurve.getPoints(CurveSegment.CurveFinalNumpoints);
+
+            // Create a tube geometry that follows that curve 
+            var geometry = new THREE.TubeGeometry(  finalCurve, 
+                                                    CurveSegment.SegmentNumpoints, 
+                                                    CurveSegment.SegmentRadius, 
+                                                    CurveSegment.SegmentNumSections, 
+                                                    false);
+            this.spanMesh = new THREE.Mesh( geometry, newMaterial );
+
+            this.root.add(this.spanMesh);
+        }
     }
 
+    //
+    // get the midsection of the set of points, starting and ending 
+    // near the 'start' and 'end' points
+    //
     findMidsection( cmPoints, start, end ) { 
         var left = 0;
         var right = 0;
         var lDist = 1000000;
         var rDist = 1000000;
+
+        // find the indices of the points closest to
+        // the 'start' and 'end' points
         for (var i=0;i<cmPoints.length; i++) {
             var lCurDist = cmPoints[i].distanceTo(start);
             var rCurDist = cmPoints[i].distanceTo(end);
@@ -100,6 +154,9 @@ class CurveSegment extends Segment {
             }
         }
 
+        // return the subset of points between
+        // the two indices found above, including the
+        // 'start' and 'end'
         if (left < right) {
             var results = [];
             results.push(start);
@@ -107,7 +164,9 @@ class CurveSegment extends Segment {
                 results.push(cmPoints[i]);
             }
             results.push(end);
+
             return results;
+
         } else {
             return cmPoints;
         }
