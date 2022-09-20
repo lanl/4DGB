bb28f38e268214d8ad93742d510eba7cb58109f7
David Honegger Rogers <dhr@lanl.gov>
2022-09-20 11:00:47 -0600

    updating test data

--- client-py/gentk/project/__init__.py
+++ client-py/gentk/project/__init__.py
@@ -7,6 +7,38 @@ class project:
         self.data = data
 
     @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def data(self, value):
+        self._name = value
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def data(self, value):
+        self._id = value
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def data(self, value):
+        self._description = value
+
+    @property
+    def interval(self):
+        return self._interval
+
+    @interval.setter
+    def data(self, value):
+        self._interval = value
+
+    @property
     def data(self):
         return self._data
 
